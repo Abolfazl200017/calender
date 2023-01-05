6f5e0a4791c947b925259db9010575f535d99850
zeraatkar <zeraatkar.abolfazl@yahoo.com>
2023-01-05 16:33:26 +0330

    config accounts app

--- calender/settings.py
+++ calender/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home',
     'bootstrap5',
+    'accounts',
 ]
 
 MIDDLEWARE = [
--- calender/urls.py
+++ calender/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls'), name='home')
+    path('', include('home.urls'), name='home'),
+    path('register/', include('accounts.urls'), name='accounts'),
 ]
