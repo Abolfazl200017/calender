ac93fbff85bdc9b5af1bac5c6c05764eb69589ab
zeraatkar <zeraatkar.abolfazl@yahoo.com>
2023-01-05 17:16:15 +0330

    config message

---
+++ accounts/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class RegisterUserForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from email import message
+from django.shortcuts import render, HttpResponse, redirect
+from .forms import RegisterUserForm
+from django.contrib.auth.models import User
 
 # Create your views here.
+def login_user(request):
+    if(request.method=='POST'):
+        f = RegisterUserForm(request.POST)
+        if f.is_valid():
+            cd = f.cleaned_data
+            User.objects.create_user(cd['username'], '', cd['password'])
+            message('تبریک', {'title':'✅'})
+            return redirect('home')
+    else:
+        f = RegisterUserForm()
+    return render(request, 'register.html', {'form': f})
+
+def signup_user(request):
+    return render(request, 'register.html', {"form":'test', 'type':'signup'})
