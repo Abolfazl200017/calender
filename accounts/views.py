68ee758d765be3f79dd5cfc7f152892e88251104
zeraatkar <zeraatkar.abolfazl@yahoo.com>
2023-01-06 17:52:35 +0330

    update code

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .forms import RegisterUserForm
-from django.contrib.auth import authenticate, logout
+from django.contrib.auth import authenticate, logout, login
 from django.contrib.auth.models import User
 from django.contrib import messages
 
@@ -19,7 +19,7 @@ def signup_user(request):
                 return redirect('home')
         else:
             f = RegisterUserForm()
-        return render(request, 'register.html', {'form': f})
+        return render(request, 'register.html', {'form': f, 'type': 'ثبت نام'})
 
 def login_user(request):
     if request.method=="POST":
@@ -28,13 +28,14 @@ def login_user(request):
             cd = f.cleaned_data
             user = authenticate(username=cd['username'], password=cd['password'])
             if user is not None:
+                login(request, user)
                 messages.success(request, 'ورود شما باموفقیت انجام شد.', '✅')
                 return redirect('home')
             else:
                 messages.error(request, 'نام کاربری یا رمز عبور اشباه میباشد.', '❌')
     else:
         f=RegisterUserForm()
-    return render(request, 'register.html', {'form': f})
+    return render(request, 'register.html', {'form': f, 'type':'ورود'})
 
 def logout_user(request):
     logout(request)
