287072df1f7820b817a2a57ae03945ad51b1d61f
zeraatkar <zeraatkar.abolfazl@yahoo.com>
2023-01-09 14:40:45 +0330

    bug fixe

--- todo/urls.py
+++ todo/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.show_todo, name='show_todo'),
     path('add/', views.add_todo, name='add_todo'),
-    path('edit/' veiws.edit_todo, name='edit_todo'),
+    path('edit/', views.edit_todo, name='edit_todo'),
 ]
