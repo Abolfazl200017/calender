72a950861b510a8fada8d9e91c52302d17d39000
zeraatkar <zeraatkar.abolfazl@yahoo.com>
2023-01-11 14:15:47 +0330

    update code

--- calender/settings.py
+++ calender/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1o$&p*6am+_-pa(w%57a6yii$i2trg8#arg(=ux*i#zvkzx(2f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
