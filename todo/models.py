155c9a556ded343577d2d78be8b77c58c929273c
zeraatkar <zeraatkar.abolfazl@yahoo.com>
2023-01-24 18:44:52 +0330

    config django_jalali to todo model

--- todo/models.py
+++ todo/models.py
@@ -3,13 +3,14 @@ from django.db import models
 # Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
+from django_jalali.db import models as jmodels
 
 # Create your models here.
 class Todo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
     body = models.TextField(blank=True)
-    date = models.DateTimeField()
+    date = jmodels.jDateField()
     time = models.CharField(max_length=2)
     private = models.BooleanField(default=False)
     exepts = models.ManyToManyField(User, related_name='firends', blank=True)
